6999790ab98eccafe6a4f1b9028ed7c3ecf4bdcb
kseniag03 <kaganina@edu.hse.ru>
2023-01-06 11:37:14 +0300

    Added JavaDoc to Graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,23 @@
 import java.util.*;
 
+/**
+ * Class graph for cycles search and topological sort realization.
+ */
 public class Graph {
+    /**
+     * Number of vertices in the graph.
+     */
     private final int vertices;
 
+    /**
+     * Adjacency list for the graph storage.
+     */
     private final ArrayList<ArrayList<Integer>> adj;
 
+    /**
+     * Constructor for Graph.
+     * @param vertices number of vertices in the graph
+     */
     Graph(int vertices) {
         this.vertices = vertices;
         adj = new ArrayList<>(vertices);
@@ -13,6 +26,11 @@ public class Graph {
         }
     }
 
+    /**
+     * Adds edge to the graph.
+     * @param start the initial vertex of the arc
+     * @param end the terminal vertex of the arc
+     */
     void addEdge(int start, int end) {
         if (start >= vertices || end >= vertices) {
             System.out.println("Vertex number is out of bounds");
@@ -21,6 +39,9 @@ public class Graph {
         }
     }
 
+    /**
+     * Prints the adjacency list of the graph.
+     */
     void printGraph() {
         for (int v = 0; v < vertices; v++) {
             System.out.println("Adj-list of vertex " + v);
@@ -31,6 +52,13 @@ public class Graph {
         }
     }
 
+    /**
+     * Searches for cycles using DFS.
+     * @param v current vertex
+     * @param used array of used vertices
+     * @param cycleDetect array of "grey" vertices (for cycle detection)
+     * @return true if the graph has cycles, false otherwise
+     */
     private boolean hasCycleDfs(int v, boolean[] used, boolean[] cycleDetect)
     {
         if (cycleDetect[v]) {
@@ -51,6 +79,10 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Checks if the graph has cycles.
+     * @return true if the graph has cycles, false otherwise
+     */
     public boolean hasCycle()
     {
         boolean[] used = new boolean[vertices];
@@ -63,6 +95,12 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Sorts graph vertices using DFS.
+     * @param v current vertex
+     * @param used array of used vertices
+     * @param order stack of current vertices order
+     */
     void topologicalSortDfs(int v, boolean[] used, Stack<Integer> order) {
         used[v] = true;
         for (var u : adj.get(v)) {
@@ -73,6 +111,10 @@ public class Graph {
         order.push(v);
     }
 
+    /**
+     * Topological sort of the graph.
+     * @return stack of sorted vertices order
+     */
     Stack<Integer> topologicalSort() {
         boolean[] used = new boolean[vertices];
         Stack<Integer> order = new Stack<>();
